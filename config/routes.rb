ac34954947f128d4c6a29987805aa90fda661f80
Miguel Soler <msoler735@gmail.com>
2018-02-28 15:49:22 -0500

    Add models for products and categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/new'
-
-  get 'products/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :products
 end
