670dc971d0fe15827f1e0ab3da5b8ad2ac707f3f
Miguel Soler <msoler735@gmail.com>
2018-03-03 19:03:27 -0500

    Finish backend for App

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,42 @@ class ProductsController < ApplicationController
   end
 
   def new
+  	@product = Product.new
   end
 
+  def create
+  	@product = Product.new(product_params)
+  	if @product.save
+  		redirect_to products_path
+  	else
+  		@errors = @product.errors.full_messages
+  		render :new
+  	end	
+  end		
+
   def edit
+    @product = Product.find(params[:id])
+  end  
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to products_path, :notice => "Product has been modified"
+    else
+      @errors = @product.errors.full_messages
+      render :edit
+    end  
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, :notice => "Product has been deleted"
+  end
+
+  private
+  def product_params
+  	params.require(:product).permit(:name, :price, category_ids: [])
   end
 end
